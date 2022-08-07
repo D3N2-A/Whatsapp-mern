cc3487c5ae9c289035261bd7e574831550466739
D3N2-A <anmolsharma999937@gmail.com>
2022-08-07 09:24:57 +0530

    cleanup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-b-BgYR_Gp2LLGKxIwyUns3g_odrPI98",
+  authDomain: "whatsapp-mern-8805a.firebaseapp.com",
+  projectId: "whatsapp-mern-8805a",
+  storageBucket: "whatsapp-mern-8805a.appspot.com",
+  messagingSenderId: "653209606934",
+  appId: "1:653209606934:web:9ef44ea7997726c2880e37",
+};

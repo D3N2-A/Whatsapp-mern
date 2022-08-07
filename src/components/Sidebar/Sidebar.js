68bdab83d4b4742be20f27e7133583943d50206d
D3N2-A <anmolsharma999937@gmail.com>
2022-08-07 23:34:51 +0530

    search container

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import "./sidebar.css";
 import React from "react";
 import Avatar from "@mui/material/Avatar";
-import { Chat, DonutLarge } from "@mui/icons-material";
+import { Chat, DonutLarge, SearchOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 const Sidebar = () => {
@@ -11,17 +11,30 @@ const Sidebar = () => {
         <Avatar
           alt="User Profile"
           src="https://media-exp1.licdn.com/dms/image/C5603AQE2QL2Us-bxkA/profile-displayphoto-shrink_400_400/0/1614710959441?e=1665619200&v=beta&t=AxEuSdCZT3o4hLpO3vSQmoAkJJ0fm1ZUkFOVarhgeH4"
+          style={{ width: "40px", height: "40px" }}
         />
         <div className="sidebar__headerRight">
           <IconButton>
-            <DonutLarge style={{ color: "white" }} />
+            <DonutLarge style={{ fontSize: "20px" }} />
           </IconButton>
           <IconButton>
-            <Chat />
+            <Chat style={{ fontSize: "20px" }} />
           </IconButton>
           <IconButton>
-            <MoreVertIcon />
+            <MoreVertIcon style={{ fontSize: "20x" }} />
           </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search or start a new chat"
+          />
         </div>
       </div>
     </div>

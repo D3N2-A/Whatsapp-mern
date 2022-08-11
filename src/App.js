68e8f020b8f925162a92ef064517d018a314c1ab
D3N2-A <anmolsharma999937@gmail.com>
2022-08-11 16:51:18 +0530

    backend link

--- src/App.js
+++ src/App.js
@@ -3,19 +3,43 @@ import Chat from "./components/Chat/Chat";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import axios from "./axios";
 const darkTheme = createTheme({
   palette: {
     mode: "dark",
   },
 });
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+  useEffect(() => {
+    const pusher = new Pusher("3a638ffc0a4c1fc6da12", {
+      cluster: "ap2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessages) => {
+      setMessages([...messages, newMessages]);
+    });
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+  console.log(messages);
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <div className="app">
         <div className="app__body">
           <Sidebar />
-          <Chat />
+          <Chat messages={messages} />
         </div>
       </div>
     </ThemeProvider>
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import SendIcon from "@mui/icons-material/Send";
 import MicIcon from "@mui/icons-material/Mic";
 import React from "react";
 import "./chat.css";
-const Chat = () => {
+const Chat = ({ messages }) => {
   return (
     <div className="chat">
       <div className="chat__header">
@@ -31,21 +31,19 @@ const Chat = () => {
         </div>
       </div>
       <div className="chat__body">
-        <p className="chat__message">
-          <span className="chat__name">Anmol</span>
-          This is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message chat__reciever">
-          <span className="chat__name">Anmol</span>
-          This is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-        <p className="chat__message">
-          <span className="chat__name">Anmol</span>
-          This is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
+        {messages.map((message) => {
+          return (
+            <p
+              className={`chat__message ${
+                message.received && "chat__reciever"
+              }`}
+            >
+              <span className="chat__name">{message.name}</span>
+              {message.message}
+              <span className="chat__timestamp">{message.timestamp}</span>
+            </p>
+          );
+        })}
       </div>
       <div className="chat__footer">
         <IconButton>
